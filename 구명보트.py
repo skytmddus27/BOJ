1e445049a9c09923c7de05f9dba9a5c526baf2df
skytmddus27 <87460767+skytmddus27@users.noreply.github.com>
2023-04-05 17:19:14 +0900

    Update 구명보트.py

--- 구명보트.py
+++ 구명보트.py
@@ -19,41 +19,41 @@ def solution(people, limit):
 제한의 중간보다 작은값, 같은값, 큰값으로 나누어 리스트를 만들었으나...
 '''
 
-def solution(people, limit):
-    people.sort()
+# def solution(people, limit):
+#     people.sort()
     
-    p1 = []
-    p5 = []
-    p9 = []
+#     p1 = []
+#     p5 = []
+#     p9 = []
     
-    for i in people:
-        if i < limit/2:
-            p1.append(i)
-        elif i == limit/2:
-            p5.append(i)
-        else:
-            p9.append(i)
+#     for i in people:
+#         if i < limit/2:
+#             p1.append(i)
+#         elif i == limit/2:
+#             p5.append(i)
+#         else:
+#             p9.append(i)
             
-    answer = len(p5) // 2
+#     answer = len(p5) // 2
     
-    p1c = p1.copy()
-    for i in p1:
-        for j in p9:
-            if i + j > limit:
-                answer += 1
-                p9.remove(j)
-            else:
-                answer += 1
-                p1c.remove(i)
-                p9.remove(j)
-                break
+#     p1c = p1.copy()
+#     for i in p1:
+#         for j in p9:
+#             if i + j > limit:
+#                 answer += 1
+#                 p9.remove(j)
+#             else:
+#                 answer += 1
+#                 p1c.remove(i)
+#                 p9.remove(j)
+#                 break
     
-    if len(p1c) > 0:
-        answer += len(p1c)
-    elif len(p5) % 2 == 1:
-        answer += 1
+#     if len(p1c) > 0:
+#         answer += len(p1c)
+#     elif len(p5) % 2 == 1:
+#         answer += 1
         
-    if len(p9) > 0:
-        answer += len(p9)
+#     if len(p9) > 0:
+#         answer += len(p9)
 
-    return answer
+#     return answer
